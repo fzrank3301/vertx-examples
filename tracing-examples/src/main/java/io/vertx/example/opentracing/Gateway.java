953c97b3ebb644854328a845ea9d55bbbb6758ee
Julien Viet <julien@julienviet.com>
2020-12-07 16:02:29 +0100

    Improve

--- tracing-examples/src/main/java/io/vertx/example/opentracing/Gateway.java
+++ tracing-examples/src/main/java/io/vertx/example/opentracing/Gateway.java
@@ -2,21 +2,16 @@ package io.vertx.example.opentracing;
 
 import io.jaegertracing.Configuration;
 import io.jaegertracing.internal.samplers.ConstSampler;
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
-import io.vertx.core.http.HttpClient;
-import io.vertx.core.http.HttpMethod;
-import io.vertx.ext.web.client.WebClient;
-import io.vertx.ext.web.client.predicate.ResponsePredicate;
+import io.vertx.example.tracing.GatewayVerticle;
 import io.vertx.tracing.opentracing.OpenTracingOptions;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-public class HelloService extends AbstractVerticle {
+public class Gateway {
 
   public static void main(String[] args) throws InterruptedException, ExecutionException, TimeoutException {
 
@@ -27,7 +22,7 @@ public class HelloService extends AbstractVerticle {
     Configuration.ReporterConfiguration reporterConfig = new Configuration.ReporterConfiguration()
       .withLogSpans(true);
 
-    Configuration config = new Configuration("HelloService")
+    Configuration config = new Configuration("Gateway")
       .withSampler(samplerConfig)
       .withReporter(reporterConfig);
 
@@ -35,30 +30,9 @@ public class HelloService extends AbstractVerticle {
       .setTracingOptions(new OpenTracingOptions(config.getTracer())
       ));
 
-    vertx.deployVerticle(new HelloService())
+    vertx.deployVerticle(new GatewayVerticle())
       .toCompletionStage()
       .toCompletableFuture()
       .get(30, TimeUnit.SECONDS);
-  }
-
-  private WebClient client;
-
-  @Override
-  public void start() {
-    client = WebClient.create(vertx);
-    vertx.createHttpServer().requestHandler(req -> {
-      client
-        .get(8082, "localhost", "/")
-        .expect(ResponsePredicate.SC_OK)
-        .expect(ResponsePredicate.contentType("text/plain"))
-        .send()
-        .onSuccess(resp -> {
-          req.response().end("Hello, here is a joke for you \"" + resp.bodyAsString() + "\"");
-        })
-        .onFailure(failure -> {
-          failure.printStackTrace();
-          req.response().end("Hello, sorry no joke for you today");
-        });
-    }).listen(8081);
   }
 }
